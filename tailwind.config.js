2f8b742422eeb5cfdc1ded6b3c64c390fdd764d4
Nox Fox <rickxxxmorty@proton.me>
2025-04-03 23:26:17 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
-/** @type
-{import('tailwindcss').Config} */
-module.exports = {
-content: [
-"./src/**/.{html,js,jsx,ts,tsx}", // Ensure all your React files are included
-],
-theme: {
-extend: {},
-},
-plugins: [],
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'deep-blue': '#0a192f',
+        'light-blue': '#172a45',
+        'slate-blue': '#8892b0',
+        'light-slate': '#ccd6f6',
+        'white-slate': '#e6f1ff',
+        'green-blue': '#64ffda',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
 };
+
+export default config;
