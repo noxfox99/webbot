71397eb8828bf4e0bd39baf41dbe103bb00ec58d
Nox Fox <rickxxxmorty@proton.me>
2025-05-12 18:47:24 +0300

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -14,6 +14,54 @@ const navigate = useNavigate();
 return (
 <div className="min-h-screen flex flex-col items-center p-5">
 <div className="w-full max-w-md mb-6 flex flex-col gap-2 sm:flex-row sm:justify-between">
+      <header className="w-full flex md:justify-center justify-between items-center flex-col p-2 bg-gradient-to-r from-gray-700 to-gray-900 mb-4">
+
+<div className="flex justify-between items-center w-full sm:mt-0 mt-5">
+  {/* Center Content */}
+  <div className="flex flex-1 justify-center items-center">
+    <a href="/">
+      <div className="flex items-center space-x-2 cursor-pointer bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 py-2 rounded-lg shadow-lg hover:opacity-75">
+        <span className="text-2xl font-semibold">X</span>
+        {/* Upload Icon SVG */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 40 20"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 3a1 1 0 011 1v6.586l3.707-3.707a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L9 10.586V4a1 1 0 011-1z"
+            clipRule="evenodd"
+          />
+          <path d="M10 13a7 7 0 100-14 7 7 0 000 14z" />
+        </svg>
+      </div>
+    </a>
+  </div>
+
+  {/* Right-aligned Telegram SVG */}
+  <div className="flex flex-[0.1] justify-end">
+    <a href="https://t.me/t" target="_blank" rel="noopener noreferrer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-label="Telegram"
+        viewBox="0 0 512 512"
+        className="w-8 h-8 cursor-pointer hover:opacity-75"
+      >
+        <rect width="512" height="512" fill="#37aee2" rx="15%"></rect>
+        <path fill="#c8daea" d="M199 404c-11 0-10-4-13-14l-32-105 245-144"></path>
+        <path fill="#a9c9dd" d="M199 404c7 0 11-4 16-8l45-43-56-34"></path>
+        <path
+          fill="#f6fbfe"
+          d="M204 319l135 99c14 9 26 4 30-14l55-258c5-22-9-32-24-25L79 245c-21 8-21 21-4 26l83 26 190-121c9-5 17-3 11 4"
+        ></path>
+      </svg>
+    </a>
+  </div>
+</div>
+
+            </header>
 <button className="bg-redButton text-white w-full sm:w-auto px-4 py-2 rounded shadow-lg">
 Войти
 </button>
